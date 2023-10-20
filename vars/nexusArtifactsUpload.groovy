f41e0d5806417c8ad686769e27328e7684b069a3
Chuck Okechukwu <85455693+chuck007-chuckietechs@users.noreply.github.com>
2023-10-20 15:41:29 -0400

    Update nexusArtifactsUpload.groovy

--- vars/nexusArtifactsUpload.groovy
+++ vars/nexusArtifactsUpload.groovy
@@ -1,3 +1,3 @@
 def call() {           
- nexusArtifactUploader artifacts: [[artifactId: 'maven-web-app', classifier: '', file: 'target/maven-web-app.war', type: 'war']], credentialsId: 'Nexus-Credentials', groupId: 'com.chuckSoftwares', nexusUrl: '3.99.154.124:8081', nexusVersion: 'nexus3', protocol: 'http', repository: 'chuckietech-snapshot-repo', version: '1.0-SNAPSHOT'
+ nexusArtifactUploader artifacts: [[artifactId: 'maven-web-app', classifier: '', file: 'target/maven-web-app.war', type: 'war']], credentialsId: 'Nexus-Credentials', groupId: 'com.chuckSoftwares', nexusUrl: '35.183.203.32:8081', nexusVersion: 'nexus3', protocol: 'http', repository: 'chuckietech-snapshot-repo', version: '1.0-SNAPSHOT'
 }
